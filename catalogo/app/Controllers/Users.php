9ef882371dfa55b9e2b70de269a0e3c54371b040
CaetanoMaldaner <caetano.maldaner@alunos.sc.senac.br>
2023-10-09 20:13:04 -0300

    Foi Criado e Atualizado o Service/Controller/Model do User

--- catalogo/app/Controllers/Users.php
+++ catalogo/app/Controllers/Users.php
@@ -3,10 +3,21 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Entities\User;
 use App\Models\UserModel;
+use App\Services\UserService;
 
 class Users extends BaseController
 {
+
+    protected $userService;
+
+    public function __construct()
+    {
+        // Injeção de dependência do serviço UserService
+        $this->userService = new UserService(model(UserModel::class));
+    }
+
     public function index()
     {
         return view('register');
@@ -14,8 +25,32 @@ class Users extends BaseController
 
     public function create()
     {
+        // Verifique se o formulário foi submetido
+        if ($this->request->getMethod() === 'post') {
+            // Validação dos dados do formulário
+            $validationRules = [
+                'email'    => 'required|valid_email',
+                'password' => 'required|min_length[6]',
+            ];
 
+            if ($this->validate($validationRules)) {
+                // Dados do formulário são válidos
+                $user = new User($this->request->getPost());
+
+                // Crie um novo usuário
+                $result = $this->userService->createUser($user);
+
+                if ($result === true) {
+                    // Sucesso na criação do usuário
+                    return redirect()->to('/dashboard'); // Redireciona para a página de sucesso
+                }
+            }
+        }
+
+        // Carrega a view de criação de usuário (formulário)
+        return view('user/create');
     }
+    
 
     public function store()
     {
@@ -61,7 +96,7 @@ class Users extends BaseController
 
                 $password = $this->request->getPost('password');
                 if (!empty($password)) {
-                    $userData['password'] = password_hash( $password , PASSWORD_BCRYPT);
+                    $userData['password'] = password_hash($password , PASSWORD_BCRYPT);
                 }
 
                 $userModel->update($id, $userData);
