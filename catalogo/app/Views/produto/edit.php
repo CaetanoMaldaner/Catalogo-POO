c860a48a415c12ba73e1b41843adfe556fc22143
thiagodasilvadebortoli <thiago_bortoli@estudante.sesisenai.org.br>
2023-11-21 21:13:26 -0300

    dhfsd

--- catalogo/app/Views/produto/edit.php
+++ catalogo/app/Views/produto/edit.php
@@ -4,7 +4,64 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('css/styles.css') ?>">
     <title>Editar Produto</title>
+
+    <style>
+        body {
+            color: #fff;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        h1 {
+            margin-bottom: 20px;
+         
+        }
+
+        form {
+            text-align: center;
+            max-width: 400px;
+            width: 100%;
+        }
+
+        label {
+            display: block;
+            margin-bottom: 5px;
+            
+        }
+
+        input[type="text"],
+        textarea,
+        select {
+            width: 100%;
+            padding: 8px;
+            margin-bottom: 15px;
+            box-sizing: border-box;
+            border-radius: 10px;
+        }
+
+        input[type="submit"] {
+            display: inline-block;
+            padding: 10px 20px;
+            text-decoration: none;
+            background-color: #845fda56;
+            color: #fff;
+            border-radius: 20px;
+            transition: background-color 0.3s ease;
+      
+        }
+
+        input[type="submit"]:hover {
+            background-color: #a172ee;
+            border-radius: 5px;
+        }
+    </style>
+    
 </head>
 
 <body>
